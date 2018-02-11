0e4bf91de83536fbb469aa7aba2e6ce7bb9e8170
Mani <manikantha92@gmail.com>
2018-02-11 14:55:29 -0500

    add hw05

---
+++ memory_server/lib/memory_web/channels/games_channel.ex
@@ -0,0 +1,49 @@
+defmodule MemoryWeb.GamesChannel do
+  use MemoryWeb, :channel
+
+  alias Memory.Game
+
+  def join("games:" <> name, payload, socket) do
+    if authorized?(payload) do
+      game = Memory.GameBackup.load(name) || Game.new()
+      socket = socket
+      |> assign(:game, game)
+      |> assign(:name, name)
+      {:ok, %{"join" => name, "game" => Game.client_view(game)}, socket}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
+  end
+
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  def handle_in("startGame", %{"card" => stateValue}, socket) do
+    game = Game.startGame(socket.assigns[:game], stateValue)
+    Memory.GameBackup.save(socket.assigns[:name], game)
+    socket = assign(socket, :game, game)
+    {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
+  end
+
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  def handle_in("matched", %{"matched" => stateValue}, socket) do
+    game = Game.isMatched(socket.assigns[:game])
+    Memory.GameBackup.save(socket.assigns[:name], game)
+    socket = assign(socket, :game, game)
+    {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
+  end
+
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  def handle_in("reset", %{"reset" => stateValue}, socket) do
+    game = Game.new()
+    Memory.GameBackup.save(socket.assigns[:name], game)
+    socket = assign(socket, :game, game)
+    {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
+  end
+
+  # Add authorization logic here as required.
+  defp authorized?(_payload) do
+    true
+  end
+end
