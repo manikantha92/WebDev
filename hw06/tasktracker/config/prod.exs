801090cc3dd526463db270a6bce28fec5fd581ee
Mani <manikantha92@gmail.com>
2018-02-19 15:06:32 -0500

    prod

--- hw06/tasktracker/config/prod.exs
+++ hw06/tasktracker/config/prod.exs
@@ -61,4 +61,7 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+#import_config "prod.secret.exs"
+
+config :memory, MemoryWeb.Endpoint,
+    secret_key_base: "THIS+IS+NOT+SECURE+DONT+USE+SESSION+COOKIES+WITHOUT+FIXING"
