47095f8abc20a839cb21a9d7aae3a9c000c68613
Mani <manikantha92@gmail.com>
2018-02-20 04:05:14 -0500

    minor fixes

--- hw06/tasktracker/config/prod.secret.exs
+++ hw06/tasktracker/config/prod.secret.exs
@@ -14,7 +14,7 @@ config :tasktracker, TasktrackerWeb.Endpoint,
 # Configure your database
 config :tasktracker, Tasktracker.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "tasktracker",
+  password: "Mani2808@",
   database: "tasktracker_prod",
   pool_size: 15
