fe131e3d6e0283fb17167b597808dba74db988bf
manikantha92 <manikantha92@gmail.com>
2018-04-02 19:56:03 -0400

    Config changes

--- hw08/tasktracker3/config/dev.exs
+++ hw08/tasktracker3/config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :tasktracker3, Tasktracker3.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "tasktracker3",
+  password: "Mani2808@",
   database: "tasktracker3_dev",
   hostname: "localhost",
   pool_size: 10
