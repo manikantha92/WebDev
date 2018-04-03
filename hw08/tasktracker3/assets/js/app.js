94b191ff033020a8a5a7b7a8ae3e10599136d41b
manikantha92 <manikantha92@gmail.com>
2018-04-02 21:10:29 -0400

    added components

--- hw08/tasktracker3/assets/js/app.js
+++ hw08/tasktracker3/assets/js/app.js
@@ -12,6 +12,8 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
+import tasktracker3_init from "./components/tasktracker";
+$(tasktracker3_init);
 
 // Import local files
 //
