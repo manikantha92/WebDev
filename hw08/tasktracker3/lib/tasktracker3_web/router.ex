e4693613d5e1d439d21c71bdeba19a9684496337
manikantha92 <manikantha92@gmail.com>
2018-04-02 22:22:54 -0400

    added sesson controller

--- hw08/tasktracker3/lib/tasktracker3_web/router.ex
+++ hw08/tasktracker3/lib/tasktracker3_web/router.ex
@@ -17,6 +17,9 @@ defmodule Tasktracker3Web.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/users", PageController, :index
+    get "/tasks", PageController, :index
+    get "/users/:id", PageController, :index
   end
 
   # Other scopes may use custom stacks.
